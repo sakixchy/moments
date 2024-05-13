c0573f2763d49956707ca5afc3cceb6a5d1965c4
Saki Chy <sakibchowdhury79@gmail.com>
2024-05-13 18:55:09 +0000

    routes to nav links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar.js';
+import Container from 'react-bootstrap/Container';
+import {Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar></NavBar>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+          <Route exact path="/signin" render={()=> <h1>Sign In</h1>} />
+          <Route exact path="/signup" render={()=> <h1>Sign Up</h1>} />
+        </Switch>
+      </Container>
    
     </div>
   );
