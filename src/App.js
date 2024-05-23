cdd28fb48b46245778a3c7209d0b1785e85a248e
Saki Chy <sakibchowdhury79@gmail.com>
2024-05-23 18:43:16 +0000

    add posts filter

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import SignUpForm from './pages/auth/SignUpForm.js';
 import SignInForm from './pages/auth/SignInForm.js';
 import PostCreateForm from './pages/posts/PostCreateForm.js';
 import PostPage from './pages/posts/PostPage.js';
+import PostsPage from './pages/posts/PostsPage.js';
+import { useCurrentUser } from './contexts/CurrentUserContext.js';
 
 
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || ""
 
   return (
  
@@ -19,7 +23,14 @@ function App() {
           <NavBar />
           <Container className={styles.Main}>
             <Switch>
-              <Route exact path="/" render={()=> <h1>Home Page</h1>} />
+              <Route exact path="/" render={
+                ()=> <PostsPage message="No results are found. Adjust the search keyword."/>} />
+               <Route exact path="/feed" render={
+                ()=> <PostsPage message="No results are found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}/>} />
+                 <Route exact path="/liked" render={
+                ()=> <PostsPage message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}/>} />
               <Route exact path="/signin" render={()=><SignInForm />} />
               <Route exact path="/signup" render={()=><SignUpForm />} />
               <Route exact path="/posts/create" render={()=><PostCreateForm />} />
